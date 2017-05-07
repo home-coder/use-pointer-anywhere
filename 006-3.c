55007b3f0167bf89e33019fa5ea5ab011fcb25d6
home-coder <one_face@sina.com>
2017-05-07 13:13:32 +0800

    优化代码分块， 分析一种错误的数组指针用法，即使没有语法毛病

--- 006-3.c
+++ 006-3.c
@@ -4,37 +4,46 @@
 
 
 typedef void (*print_arry)(const char *arry, const int len);
-typedef int(arryp)[10];
+typedef int(arryp)[3];
 int main()
 {
 	//1.p-->int
-	int i = 4;
-	int *pi = &i;
-	printf("*pi =%d\n", *pi);
-
-	//2.p-->char
-	char c = 'a';
-	char *pc = &c;
-	printf("*pc =%c\n", *pc);
-
-	//3.1 p-->int[]
-	int arri[10] = {1, 2, 3};
-#if 0	
-	int *parri = arri;
-	int ix = 0;
-	for (; ix < sizeof(arri)/sizeof(arri[0]); ix++) {
-		printf("*parri[%d] =%d\n", ix, *(parri + ix));
+	{
+		int i = 4;
+		int *pi = &i;
+		printf("*pi =%d\n", *pi);
 	}
-#endif	
+	//2.p-->char
+	{
+		char c = 'a';
+		char *pc = &c;
+		printf("*pc =%c\n", *pc);
+	}
+	//3.1 p-->int[]
+	{
+		int arri[10] = {1, 2, 3};
+		int *parri = arri;
+		int ix = 0;
+		for (; ix < sizeof(arri)/sizeof(arri[0]); ix++) {
+			printf("*parri[%d] =%d\n", ix, *(parri + ix));
+		}
+	}
 	//3.2 p-->int[]
-	int (*parri)[] = &arri;
-	#define SIZE(x) (sizeof(x)/sizeof(*x))
-	int ix = 0;
-	arryp *p = &arri; 
-	for (; ix < 3; ix++) {
-		(*p)[i] = 3 - i;
-	}	
-	printf("%d\n", (*p)[2]);
+	{
+		int arri[10] = {1, 2, 3};
+		int (*parri)[] = &arri;
+#define SIZE(x) (sizeof(x)/sizeof(*x))
+		int ix = 0;
+		for (; ix < 3; ix++) {
+			printf("一维数组，还用二维的数组指针，这种方式太多余\n");		
+		}
+	}
+	//3.3 p-->int[][]
+	{
+		int arri[][3] = {{1, 3, 4}, {9, 8, 3}, {9, 0, 9}};
+		arryp *p = arri; //首元素：是一个一维数组{1, 3, 4}	
+
+	}
 	//4.1 p-->char[]
 	char arrc[] = {'a', 'b', 'c'};
 	char *parrc = arrc;
