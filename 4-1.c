8938007507af7dcb88f75ddc196f051c919b60cc
jiangxiujie <jiangxiujie@cantv.cn>
2017-05-10 18:13:46 +0800

    006中的4-x专题，字符数组的遍历

---
+++ 4-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//4.x
+int main()
+{
+//4.1
+	{
+		char c[] = "hellocoder";
+		char *p = c;
+		for (; *p !=0; p++) {
+			printf("%c ", *p);
+		}
+		printf("\n");
+	}
+
+	{
+		char 
+	
+	}
+
+	return 0;
+}
